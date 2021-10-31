8242e467e45749660201b3c65563503b41ad8d70
unknown <christineilagan02@gmail.com>
2021-10-31 23:34:08 +0800

    Second commit

--- No3.py
+++ No3.py
@@ -1,8 +1,6 @@
-enterMoney = int(input("you will enter the amount of money you have."))
+money = int(input("you will enter the amount of money you have."))
 priceApple = int(input("ask for the price of an apple."))
 
-maximumApple = int(input("maximum number of apples that you can buy."))
-
-appleTotal = priceApple * maximumApple
-moneyLeft = enterMoney - appleTotal
-print(f"You can buy {maximumApple} apples and our change is {moneyLeft} pesos.")
+appleTotal = money // priceApple
+change = money % appleTotal
+print(f"You can buy {appleTotal} apples and our change is {change} pesos.")
